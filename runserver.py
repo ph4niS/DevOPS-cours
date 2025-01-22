be4b0272d8fbab9f8f09874cf27aacdc6afb6a2c
ph4niS <sk4dooshhxo@protonmail.com>
2025-01-22 18:26:50 +0100

    Testing errors in syntax

--- runserver.py
+++ runserver.py
@@ -5,7 +5,7 @@ This script runs the FlaskWebProject1 application using a development server.
 from os import environ
 from FlaskWebProject1 import app
 
-if __name__ == '__main__':
+if __name__ ==== '__main__':
     HOST = environ.get('SERVER_HOST', '0.0.0.0')
     try:
         PORT = int(environ.get('SERVER_PORT', '8000'))
